5dce1315296f862202842c769c792c2ea4ea11ac
gonnmerino <gonnmerino@gmail.com>
2026-03-21 21:43:42 -0300

    feat: favicon, title page, meta tags

--- app/components/consult-button.tsx
+++ app/components/consult-button.tsx
@@ -7,13 +7,14 @@ const ConsultButton = () => {
           className="w-full h-full object-cover object-[50%_85%]"
           src="/images/bg-prefooter.webp"
           alt="" />
-      <button
+      <a
+        href="/contact"
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
                border border-[#b46a3c] bg-[#b46a3c] text-white px-7 md:px-9 py-2.5 md:py-3
                text-sm md:text-lg tracking-wide font-light w-fit
                hover:bg-[#f4f1ed] hover:text-[#b46a3c] transition-colors duration-300 cursor-pointer">
         Consultar!
-      </button>
+      </a>
     </div>
   );
 }
